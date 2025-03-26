6c6ccfe600629b276eed0089c8b4855f6487e9c4
saurabhkirgat09 <saurabhkirgat1999@gmail.com>
2025-03-26 14:36:21 +0530

    add new code

---
+++ StreamAPICodingQuestions/src/main/java/org/example/FindMaximumSalaryFromDepartment.java
@@ -0,0 +1,96 @@
+package org.example;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FindMaximumSalaryFromDepartment {
+
+    public static Map<String, Employee> getHighestSalaryByDept(List<Employee> employees) {
+        Map<String, Employee> highestSalaries = employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment,
+                        Collectors.collectingAndThen(
+                                Collectors.maxBy(Comparator.comparing(Employee::getSalary)), Optional::get)
+                        ));
+        return highestSalaries;
+    }
+
+    public static void main(String[] args) {
+        List<Employee> employees = Arrays.asList(
+                new Employee(1, "Alice", 70000, "IT"),
+                new Employee(2, "Bob", 80000, "HR"),
+                new Employee(3, "Charlie", 75000, "IT"),
+                new Employee(4, "David", 65000, "Finance"),
+                new Employee(5, "Eve", 90000, "HR")
+        );
+
+
+        Map<String, Employee> highestSalaries = getHighestSalaryByDept(employees);
+        highestSalaries.forEach((dept, emp) ->
+                System.out.println(dept + " -> " + emp));
+
+        List<String> empNames = employees.stream()
+                .filter(e-> e.getSalary() > 40000)
+                .map(Employee::getName)
+                .toList();
+
+        System.out.println("List of employees having salaries more than 40K :: " + empNames);
+    }
+
+
+    static class Employee {
+
+        public Employee(int id, String name, double salary, String department) {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+            this.department = department;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public double getSalary() {
+            return salary;
+        }
+
+        public void setSalary(double salary) {
+            this.salary = salary;
+        }
+
+        public String getDepartment() {
+            return department;
+        }
+
+        public void setDepartment(String department) {
+            this.department = department;
+        }
+
+        int id;
+        String name;
+        double salary;
+        String department;
+
+        @Override
+        public String toString() {
+            return "Employee{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", salary=" + salary +
+                    ", department='" + department + '\'' +
+                    '}';
+        }
+    }
+}
