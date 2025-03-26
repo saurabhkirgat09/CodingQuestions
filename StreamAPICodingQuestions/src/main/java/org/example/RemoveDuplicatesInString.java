9b05b0349422a34f85e7da55117d40e9eba5fd11
saurabhkirgat09 <saurabhkirgat1999@gmail.com>
2025-03-27 01:44:28 +0530

    1. Add Solution to remove duplicate characters from String and return

---
+++ StreamAPICodingQuestions/src/main/java/org/example/RemoveDuplicatesInString.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.stream.Collectors;
+
+public class RemoveDuplicatesInString {
+    public static void main(String[] args) {
+        String input = "bcdedbsfesd";
+        System.out.println(" String after removing duplicates: " + removeDuplicateChars(input));
+    }
+
+    private static String removeDuplicateChars(String input) {
+        return input.chars() //provides InputStream
+                .distinct()  // Apply distinct to remove duplicates
+                .mapToObj(c-> String.valueOf((char) c)) //convert ascii codes into char first then String
+                .collect(Collectors.joining()); //Join all the characters to Complete String
+    }
+
+}
