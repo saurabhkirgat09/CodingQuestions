14c96091e476678d5932ea3b86b280eed8366337
saurabhkirgat09 <saurabhkirgat1999@gmail.com>
2025-03-30 01:53:06 +0530

    1. Add Code to find the average of the given list

--- StreamAPICodingQuestions/src/main/java/org/example/FindSumOfAllIntegers.java
+++ StreamAPICodingQuestions/src/main/java/org/example/FindSumOfAllIntegers.java
@@ -8,11 +8,24 @@ public class FindSumOfAllIntegers {
     public static void main(String[] args) {
         List<Integer> numList = List.of(2,3,43,54,34,7,65,44,35,75,54,65,54,78,97);
         Optional<Integer> result =  getSumOfNumbers(numList);
+        double average = getAverage(numList);
         result.ifPresent(value -> System.out.println("Sum of All Integers in list is: " + value));
+        System.out.println("Average of Given list is: " +average);
     }
 
+    //Logic to get Sum of Numbers
     private static Optional<Integer> getSumOfNumbers(List<Integer> numList) {
        return numList.stream()
                 .reduce(Integer::sum);
     }
+
+    //Logic to get print average
+    private static double getAverage(List<Integer> numList) {
+        return numList.stream()
+                .mapToInt(e-> e)
+                .average()
+                .orElse(0.0);
+    }
+
+
 }
