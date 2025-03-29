5dfb7452dde35f23c0218230f85e38b69d253f77
saurabhkirgat09 <saurabhkirgat1999@gmail.com>
2025-03-30 02:29:46 +0530

    1. Add Code to Find the square of Numbers and filter the numbers greater than 100 and the find the average of List

--- StreamAPICodingQuestions/src/main/java/org/example/FindSumOfAllIntegers.java
+++ StreamAPICodingQuestions/src/main/java/org/example/FindSumOfAllIntegers.java
@@ -8,9 +8,14 @@ public class FindSumOfAllIntegers {
     public static void main(String[] args) {
         List<Integer> numList = List.of(2,3,43,54,34,7,65,44,35,75,54,65,54,78,97);
         Optional<Integer> result =  getSumOfNumbers(numList);
-        double average = getAverage(numList);
         result.ifPresent(value -> System.out.println("Sum of All Integers in list is: " + value));
+
+        double average = getAverage(numList);
         System.out.println("Average of Given list is: " +average);
+
+        double averageOfSquareAndFilter = findSquareFilterAndAverage(numList);
+        System.out.println("The Average of List with Numbers having Square value more than 100: " +
+                averageOfSquareAndFilter);
     }
 
     //Logic to get Sum of Numbers
@@ -25,6 +30,17 @@ public class FindSumOfAllIntegers {
                 .mapToInt(e-> e)
                 .average()
                 .orElse(0.0);
+    }
+
+    //Find the square of Numbers and filter the numbers greater than 100 and the find the average of List
+    private static double findSquareFilterAndAverage (List<Integer> numList) {
+        return numList.stream()
+                .map(n-> n*n)
+                .filter(n-> n>100)
+                .mapToInt(e->e)
+                .average()
+                .orElse(0.0);
+
     }
 
 
