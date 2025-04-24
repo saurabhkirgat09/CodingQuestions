6c9c302b80438bec0fdeeec3e202ab3a986d22ba
saurabhkirgat09 <saurabhkirgat1999@gmail.com>
2025-04-24 20:52:23 +0530

    1. Add code to find first non duplicate character

--- StreamAPICodingQuestions/src/main/java/org/example/FindDuplicateCharactersInString.java
+++ StreamAPICodingQuestions/src/main/java/org/example/FindDuplicateCharactersInString.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -8,16 +9,25 @@ public class FindDuplicateCharactersInString {
 
     public static void main(String[] args) {
 
-        for (Map.Entry<Character, Long> result :findRepeatingCharactersWithMap("programming").entrySet()) {
+        // logic to get the occurrence of duplicate characters
+        for (Map.Entry<Character, Long> result :findRepeatingCharactersWithMap("abacabddcefgf").entrySet()) {
             if (result.getValue() > 1) {
                 System.out.println("Character: " + result.getKey() + " Occurrence: " +result.getValue() + " times");
+            }
+        }
+
+        //Logic to get first non occurring character
+        for (Map.Entry<Character,Long> result : findRepeatingCharactersWithMap("abacabddcefgf").entrySet()) {
+            if (result.getValue() == 1) {
+                System.out.println("The first non ocurring character: "+ result.getKey());
+                break;
             }
         }
     }
 
 
     private static Map<Character, Long> findRepeatingCharactersWithMap(String input){
-        Map<Character, Long> result = new HashMap<>();
+        Map<Character, Long> result = new LinkedHashMap<>();
         for (char ch : input.toCharArray()) {
             result.put(ch, result.getOrDefault(ch, 0L) + 1);
         }
